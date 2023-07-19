29fd82de2a5c96e628966f8f86c57384c0a88d7a
saidazimov720 <saidazimovnurbek01@gmail.com>
2023-07-19 14:43:09 +0500

    finddublicates

---
+++ script.js
@@ -0,0 +1,30 @@
+const array = [1, 2, 3, 2, 5, 1, 6, 7, 6, 8, 9];
+  const takror = Takrorlanuvchi(array);
+
+function Takrorlanuvchi(arr) {
+    const takror = {};
+    
+    for (const element of arr) {
+      if (takror[element]) {
+        takror[element]++;
+      } else {
+        takror[element] = 1;
+      }
+    }
+    
+    const natija = [];
+    
+    for (const key in takror) {
+      if (takror.hasOwnProperty(key) && takror[key] > 1) {
+        natija.push(key);
+      }
+    }
+    
+    return natija;
+  }
+  
+
+  
+  
+  console.log('Takrorlanuvchi elementlar:', takror);
+  
