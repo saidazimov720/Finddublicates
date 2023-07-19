caae28faa275209723a7d48076ae4eb14740ed0e
saidazimov720 <saidazimovnurbek01@gmail.com>
2023-07-19 23:09:52 +0500

    edit

--- script.js
+++ script.js
@@ -12,42 +12,12 @@ function takrorlanuvchi() {
       counts[element] = 1;
     }
   }
-  var resultDiv = document.getElementById("result");
-  resultDiv.innerHTML = "<h3>Duplicate Elements:</h3>";
+  var natijalar = document.getElementById("natija");
+  natijalar.innerHTML = "<h3>Takrorlanuvchi elementlar:</h3>";
 
   for (var key in counts) {
     if (counts[key] > 1) {
-      resultDiv.innerHTML += key + "<br>";
+      natijalar.innerHTML += key + "<br>";
     }
   }
-}
-// const array = [1, 2, 3, 2, 5, 1, 6, 7, 6, 8, 9,5, 4, 8, 7, 1, 5];
-//   const takror = Takrorlanuvchi(array);
-
-// function Takrorlanuvchi(arr) {
-//     const takror = {};
-    
-//     for (const element of arr) {
-//       if (takror[element]) {
-//         takror[element]++;
-//       } else {
-//         takror[element] = 1;
-//       }
-//     }
-    
-//     const natija = [];
-    
-//     for (const key in takror) {
-//       if (takror.hasOwnProperty(key) && takror[key] > 1) {
-//         natija.push(key);
-//       }
-//     }
-    
-//     return natija;
-//   }
-  
-
-  
-  
-//   console.log('Takrorlanuvchi elementlar:', takror);
-  
+}  
